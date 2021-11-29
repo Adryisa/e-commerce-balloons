4c326e8d04c722bc8c3e9fad2581ca5f4f9689f9
Adriana Isabel Ortiz Tosone <adryisa26@gmail.com>
2021-11-29 14:53:24 +0100

    Add the connection to the db func

---
+++ backend/config/dbConnection.js
@@ -0,0 +1,15 @@
+const mongoose = require('mongoose')
+const dotenv = require('dotenv')
+
+dotenv.config() 
+
+async function dbConnection() {
+    const uri = process.env.NODE_ENV === 'test' ? 
+    process.env.MONGO_URI_TEST : process.env.MONGO_URI
+
+    const response = await mongoose.connect(uri)
+
+    return response
+}
+
+module.exports = dbConnection
