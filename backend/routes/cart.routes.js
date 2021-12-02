eef39ae2111ac199247d0d333ccf84706e098d55
Adriana Isabel Ortiz Tosone <adryisa26@gmail.com>
2021-12-02 13:38:59 +0100

    Add cart routes

---
+++ backend/routes/cart.routes.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const {
+  getCartById,
+  addBalloonToCart,
+  updateBalloonAmountCart,
+  deleteBalloonCart,
+} = require('../controller/cart.controller');
+
+const router = express.Router();
+
+router
+  .route('/:cartId/balloon/:balloonId')
+  .post(addBalloonToCart)
+  .patch(updateBalloonAmountCart)
+  .delete(deleteBalloonCart);
+router.route('/:id').get(getCartById);
