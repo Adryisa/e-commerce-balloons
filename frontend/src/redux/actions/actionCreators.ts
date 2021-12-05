d34945525b71a9a4831b3ef33c25d240b8dc04aa
Adriana Isabel Ortiz Tosone <adryisa26@gmail.com>
2021-12-05 18:00:37 +0100

    Add load balloons on the actioncreator

--- frontend/src/redux/actions/actionCreators.ts
+++ frontend/src/redux/actions/actionCreators.ts
@@ -6,7 +6,13 @@ const urlBase = 'http://localhost:3200/api/'
 
 
 export function loadBalloons() {
-   const urlApi = `${urlBase}/balloons`
+  const urlApi = `${urlBase}/balloons`
 
-   return async (useDispatch)
+   return async (dispatch: any) => {
+      const { data } = await axios.get(urlApi) 
+      dispatch({
+         type: balloonsAndCartActionTypes.LOAD_BALLOONS,
+         payload: data
+      })
+   }
 }
