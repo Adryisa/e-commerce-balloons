db764aacdce1b7d6fba8d83a8292cd8327984d4e
Adriana Isabel Ortiz Tosone <adryisa26@gmail.com>
2021-12-05 16:24:41 +0100

    Add actionType: load_balloons

---
+++ frontend/src/redux/actions/actionTypes.ts
@@ -0,0 +1,10 @@
+export interface actionTypesInterface {
+    LOAD_BALLOONS: string
+}
+
+const balloonsAndCartActionTypes: actionTypesInterface = {
+    LOAD_BALLOONS: 'LOAD_BALLOONS'
+}
+
+
+export default balloonsAndCartActionTypes
