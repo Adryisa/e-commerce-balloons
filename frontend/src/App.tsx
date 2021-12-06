74499864f07a3af094c7915eb9fa522e56e831cf
Adriana Isabel Ortiz Tosone <adryisa26@gmail.com>
2021-12-06 12:44:14 +0100

    Starting over

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,12 +1,10 @@
-import * as React from 'react';
 import './App.scss';
 
-// eslint-disable-next-line
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        BALLOONS
+        Bprobando 
       </header>
     </div>
   );
