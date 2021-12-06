4cb1d857b86aec34fa750a0abd6f2a1995b1aa11
Adriana Isabel Ortiz Tosone <adryisa26@gmail.com>
2021-12-06 12:00:32 +0100

    Testing

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,10 +1,12 @@
-import React from 'react';
+import * as React from 'react';
+import './App.scss';
 
+// eslint-disable-next-line
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-          BALLOONS
+        BALLOONS
       </header>
     </div>
   );
--- frontend/src/redux/store/store.ts
+++ frontend/src/redux/store/store.ts
@@ -1,6 +1,6 @@
 import { applyMiddleware, createStore, compose } from 'redux';
-import thunk from 'redux-thunk'
-// import rootReducer from "../reducers/indexReducer";
+import thunk from 'redux-thunk';
+import rootReducer from '../reducers/index';
 
 declare global {
   interface Window {
@@ -9,13 +9,13 @@ declare global {
 }
 
 export default function configureStore(initialState: {}) {
-    const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__
+  const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__
   || compose;
   return createStore(
-      rootReducer,
-      initialState,
-       composeEnhancers(applyMiddleware(thunk)),
-  )
+    rootReducer,
+    initialState,
+    composeEnhancers(applyMiddleware(thunk)),
+  );
 }
 
 const store = configureStore({});
