35df2e589dc1454c65b26588769b5ac366870582
Adriana Isabel Ortiz Tosone <adryisa26@gmail.com>
2021-12-06 19:37:51 +0100

    Add header scss

--- frontend/src/components/Header/header.tsx
+++ frontend/src/components/Header/header.tsx
@@ -3,13 +3,17 @@ import React from 'react'
 import partyando from '../../assets/logoPartyando.svg'
 import cart from '../../assets/cart.svg'
 import logUser from '../../assets/user-logo.svg'
+import './header.scss'
 
 const Header = () => {
     return (
         <header className='header'>
             <h1 className='header__title'>Partyando
-            <img src={partyando} alt="logo partyando events" className='header__title-img'/>
             </h1>
+            <div className='header__logo-container'>            
+            <img src={partyando} alt="logo partyando events" className='header__title-logo'/>
+            </div>
+
             <section className='header__login-cart'>
             <img src={cart} alt="ir al carrito" />
             <img src={logUser} alt="link para logearte" />
