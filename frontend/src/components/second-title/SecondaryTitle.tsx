f8950a8b95435d7b5fa44ffc294a66cb4be6bb35
Adriana Isabel Ortiz Tosone <adryisa26@gmail.com>
2021-12-07 09:59:48 +0100

    Add secondarytitle component

---
+++ frontend/src/components/second-title/SecondaryTitle.tsx
@@ -0,0 +1,20 @@
+import React from 'react'
+
+const SecondaryTitle = () => {
+    return (
+        <div>
+        <p className='secondary-title'>
+        Find your products: 
+        </p>
+        <p className='secondary-title__shop'>
+        SHOP
+        </p>
+        <p className='secondary-title__cart'>
+        CART 
+        </p>
+        <div className='secondary-title__bar' />
+        </div>
+    )
+}
+
+export default SecondaryTitle
--- frontend/src/pages/Home/Home.tsx
+++ frontend/src/pages/Home/Home.tsx
@@ -2,6 +2,8 @@ import React from 'react'
 import {useDispatch, useSelector} from 'react-redux'
 import { useEffect } from 'react'
 import { loadBalloons } from '../../redux/actions/actionCreators'
+import './home.scss'
+import SecondaryTitle from '../../components/second-title/SecondaryTitle'
 
 const Home = () => {
 
@@ -14,18 +16,19 @@ const dispatch = useDispatch()
 
 // console.log(balloons)
 
+// put an oncliclk on the image to go to shop list
+
     return (
         <nav className='navigation'>
-            <p className='navigation__text'>
-                Find your products: 
-            </p>
-            <div className='navigation__bar' />
+                    <SecondaryTitle />
             <ul className='navigation__list'>
-            <li className='navigation__list-item'>
-                Balloons
+            <li className='navigation__list-item' >
+                <img src="" alt="balloons image" className='navigation__list-img'/>
+               <p className='navigation__list-text'> Balloons</p>
             </li>
             <li className='navigation__list-item'>
-                Accesories
+                <p className='navigation__list-text' >Accesories</p> 
+                <img src="" alt="accesories image" className='navigation__list-img'/>
             </li>
             </ul>
         </nav>
