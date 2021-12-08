da5c598a79d2086a51b0371101490450ef88a7a0
Adriana Isabel Ortiz Tosone <adryisa26@gmail.com>
2021-12-08 10:31:01 +0100

    Add actioncreator load_cart

--- frontend/src/redux/actions/actionCreators.ts
+++ frontend/src/redux/actions/actionCreators.ts
@@ -16,7 +16,7 @@ export function loadBalloons() {
             type: balloonsAndCartActionTypes.LOAD_BALLOONS,
             balloons: data
          })
-         
+
       } catch (err) {
          dispatch({
             type: balloonsAndCartActionTypes.FAILED_LOAD_BALLOONS,
@@ -25,3 +25,27 @@ export function loadBalloons() {
       }
    }
 }
+
+export function loadCart(id: any) {
+   const urlApi = `${urlBase}/cart/${id}`
+   const token = JSON.parse(localStorage.getItem('user') || '{}');
+
+   return async (dispatch: AppDispatch) => {
+         try {
+      const { data } = await axios.get(urlApi, {
+         headers: {
+            Authorization: `Bearer ${token.token}`
+         }
+      })
+      dispatch({
+         type: balloonsAndCartActionTypes.LOAD_CART,
+         cart: data
+      })
+   } catch (err) {
+      dispatch({
+         type: balloonsAndCartActionTypes.FAILED_LOAD_CART, 
+         err
+      })
+      }
+   }    
+}  
--- frontend/src/redux/actions/actionTypes.ts
+++ frontend/src/redux/actions/actionTypes.ts
@@ -2,6 +2,7 @@ export interface actionTypesInterface {
     LOAD_BALLOONS: string,
     FAILED_LOAD_BALLOONS: string,
     LOAD_CART: string
+    FAILED_LOAD_CART: string
 
 
 }
@@ -9,7 +10,8 @@ export interface actionTypesInterface {
 const balloonsAndCartActionTypes: actionTypesInterface = {
     LOAD_BALLOONS: 'LOAD_BALLOONS',
     FAILED_LOAD_BALLOONS: 'FAILED_LOAD_BALLOONS',
-    LOAD_CART: 'LOAD_CART'
+    LOAD_CART: 'LOAD_CART',
+    FAILED_LOAD_CART: 'FAILED_LOAD_CART'
 }
 
 
