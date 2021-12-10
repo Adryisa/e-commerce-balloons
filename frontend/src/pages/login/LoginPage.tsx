99f3b8e41ed56b779288c13d56b00b19ddb5a157
Adriana Isabel Ortiz Tosone <adryisa26@gmail.com>
2021-12-10 17:11:54 +0100

    Add form log in style

--- frontend/src/pages/login/LoginPage.tsx
+++ frontend/src/pages/login/LoginPage.tsx
@@ -2,26 +2,23 @@ import React from 'react'
 import user from '../../assets/user-logo.svg'
 import Login from '../../components/Login/login'
 import './loginPage.scss'
+import { Link } from 'react-router-dom'
 
 const LoginPage = () => {
     return (
         <section className='login'>        
         <p className='page-title'> LOGIN </p> 
         <p className='bar'></p>
-
-        <div className='logo-container'>  
-
+        <div className='logo-container'>
         <div className='logo-container__circle'>
-            
         <img src={user} alt="user icon" className='logo-container__icon'/>
         </div>
         </div>
-
-
         <Login />
 
         <div className='register-container'>
-            <p className='register-container__text'>IF YOU ARE NOT REGISTER</p> <button className='register-container__button'>REGISTER</button>
+            <p className='register-container__text'>IF YOU ARE NOT REGISTERED</p> 
+        <Link to='/register'> <button className='register-container__button'>REGISTER</button></Link> 
         </div>
         </section>
 
