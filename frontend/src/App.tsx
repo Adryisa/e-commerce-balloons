6d9416ff963729fef8ce33edb3e8677118a81083
Adriana Isabel Ortiz Tosone <adryisa26@gmail.com>
2022-01-19 10:18:08 +0100

    Update listShop page

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,35 +1,33 @@
-import React from 'react';
-import './App.scss'
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import Header from './components/header/header';
-import Sidebar from './components/sidebar/Sidebar';
-import Home from './pages/home/Home';
-import ListShop from './pages/List-shop/ListShop';
-import Footer from './components/footer/Footer';
-import Cart from './pages/cart/Cart';
-import LoginPage from './pages/login/LoginPage';
-import RegisterPage from './pages/register/registerPage';
-
+import React from "react";
+import "./App.scss";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Header from "./components/Header/header";
+import Sidebar from "./components/sidebar/Sidebar";
+import Home from "./pages/Home/Home";
+import ListShop from "./pages/List-shop/ListShop";
+import Footer from "./components/footer/Footer";
+import Cart from "./pages/cart/Cart";
+import LoginPage from "./pages/login/LoginPage";
+import RegisterPage from "./pages/register/registerPage";
 
 function App() {
   return (
-    <BrowserRouter>   
-    <main  className="main" id="main">
-      <Sidebar />
-      <div className="app">
-        <Header />
-        <Routes>
-        <Route path='/' element={<Home/>} />
-        <Route path='/login' element={<LoginPage />} />
-        <Route path='/register' element={<RegisterPage />} />
-        <Route path='/shop' element={<ListShop />} />
-        <Route path='/cart/:id' element={<Cart />} />
-        </Routes>
-        <Footer />
-    </div> 
-    </main>
+    <BrowserRouter>
+      <main className="main" id="main">
+        <Sidebar />
+        <div className="app">
+          <Header />
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/login" element={<LoginPage />} />
+            <Route path="/register" element={<RegisterPage />} />
+            <Route path="/shop" element={<ListShop />} />
+            <Route path="/cart/:id" element={<Cart />} />
+          </Routes>
+          <Footer />
+        </div>
+      </main>
     </BrowserRouter>
- 
   );
 }
 
